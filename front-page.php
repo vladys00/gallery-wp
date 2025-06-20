4940285aeb2d2cbe695013888feef0bffa538934
Vladys K <vldk0022@gmail.com>
2025-06-20 19:17:10 +0200

    updated header and created front page

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+<h1>This is the front page</h1>
+<div>
+    <p>Here we'll have a gallery slider of the exhibitions</p>
+</div>
+<?php
+ get_footer();
+?>
--- functions.php
+++ functions.php
@@ -4,7 +4,14 @@ function gallery_assets() {
     wp_enqueue_style('mytheme-style', get_template_directory_uri() . '/build/css/index.css', [], null);
     wp_enqueue_script('mytheme-script', get_template_directory_uri() . '/build/js/index.js', ['jquery'], null, true);
     wp_enqueue_script('alert-script', get_template_directory_uri() . '/build/js/alert.js', ['jquery'], null, true);
+    
 }
 
 add_action('wp_enqueue_scripts', 'gallery_assets');
+
+function gallery_features(){
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'gallery_features');
 ?>
