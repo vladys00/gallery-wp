b4452604154fc1606d8bad26ae0be74b90124699
Vladys K <vldk0022@gmail.com>
2025-06-20 19:27:14 +0200

    basic footer layout

--- footer.php
+++ footer.php
@@ -1,5 +1,35 @@
-<footer>
-    <p>This is the footer</p>
+<footer class="footer">
+    <div class="footer__social">
+        <p class="footer__title">Follow us</p>
+        <ul class="footer__social-list">
+            <li class="footer__social-item">facebook icon</li>
+            <li class="footer__social-item">instagram icon</li>
+            <li class="footer__social-item">twitter icon</li>
+            <li class="footer__social-item">youtube icon</li>
+        </ul>
+    </div>
+
+    <div class="footer__links">
+        <div class="footer__section">
+            <p class="footer__title">About</p>
+            <ul class="footer__list">
+                <li class="footer__item"><a href="" class="footer__link">The gallery</a></li>
+                <li class="footer__item"><a href="" class="footer__link">Job opportunities</a></li>
+            </ul>
+        </div>
+        <div class="footer__section">
+            <p class="footer__title">Support</p>
+            <ul class="footer__list">
+                <li class="footer__item"><a href="" class="footer__link">Terms and conditions</a></li>
+                <li class="footer__item"><a href="" class="footer__link">Privacy policy</a></li>
+                <li class="footer__item"><a href="" class="footer__link">Accessibility</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="footer__copyright">
+        <p class="footer__text">@ Almine Custom | All rights reserved</p>
+    </div>
 </footer>
 <? wp_footer(); ?>
 
