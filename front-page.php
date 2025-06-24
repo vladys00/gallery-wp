71cc216ea7e38c4c8c93e0b6dfa4065a8dd33794
Vladys K <vldk0022@gmail.com>
2025-06-24 03:53:09 +0200

    dislaying custom quqery in the front page

--- front-page.php
+++ front-page.php
@@ -4,7 +4,23 @@ get_header();
 <div class="front-container">
     <h1>This is the front page</h1>
     <div>
-        <p>Here we'll have a gallery slider of the exhibitions</p>
+        <?php
+        $args = [
+                'post_type' => 'exhibition', 
+                'posts_per_page' => 5,       
+            ];
+        $exhibitions = new WP_Query($args);
+
+        if ($exhibitions -> have_posts()){
+            while ($exhibitions -> have_posts()){
+            $exhibitions -> the_post()?>
+            <h2><?php the_title();  ?></h2>
+        <?php }
+           wp_reset_postdata();
+                } else {
+                    echo 'No exhibitions found';
+                }
+        ?>
     </div>
 
 </div>
