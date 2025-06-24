be1ee864f8de312d0e2ba0f6635527adc6f33e82
Vladys K <vldk0022@gmail.com>
2025-06-24 23:14:53 +0200

    adde theme files fot news archive and single

---
+++ archive-news.php
@@ -0,0 +1,14 @@
+<?php
+ get_header();
+?>
+<div class="container">
+    <?php
+        while (have_posts()) {
+            the_post();
+            the_title('<h2><a href="'. get_permalink().'">','</a></h2>');
+        }
+     ?>
+</div>
+<?php
+ get_footer();
+ ?>
---
+++ single-news.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+?>
+<main>
+    <div class="image-container">
+        <?php
+            if (has_post_thumbnail()) {
+            $thumb_url = get_the_post_thumbnail_url(get_the_ID(), 'large'); // or 'medium', 'full', etc.
+            echo '<img src="' . esc_url($thumb_url) . '" alt="' . esc_attr(get_the_title()) . '" class="my-custom-thumbnail">';
+            }
+        ?>
+    </div>
+ 
+    <header>
+        <h1>
+            <?php the_title();?> 
+        </h1>
+        <div>
+            <p><?php echo get_field('start_date') ?>-<?php echo get_field('end_date') ?></p> | <span><?php echo get_field('location') ?></span>
+        </div>
+        <div>
+            <p><?php echo get_field('opening_info') ?></p>
+        </div>
+    </header>
+    <section>
+        <div>
+           <?php
+                $blocks = parse_blocks(get_the_content());
+
+                foreach ($blocks as $block) {
+                    if ($block['blockName'] === 'core/paragraph') {
+                        echo apply_filters('the_content', render_block($block));
+                    }
+                }
+            ?>
+        </div>
+    </section>    
+</main>
+<?php
+ get_footer();
+?>
