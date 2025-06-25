6c6d32c210871cd6a28152faab5a9b210a003e55
Vladys K <vldk0022@gmail.com>
2025-06-25 16:58:54 +0200

    styled exhibitions

--- archive-exhibition.php
+++ archive-exhibition.php
@@ -2,12 +2,24 @@
 get_header();
 ?>
 <div class="container">
-    <?php
-        while (have_posts()) {
-            the_post();
-            the_title('<h2><a href="'. get_permalink().'">','</a></h2>');
-        }
-     ?>
+    <div class="filter">
+        <p>Current</p>
+    </div>
+    <div class="grid">
+         <?php
+                while (have_posts()) {
+                the_post();?>
+                <a class="exhibition-grid__card-link" href="<?php the_permalink(); ?>" >
+                    <div>
+                        <img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="Thumbnail">
+                        <p><?php echo get_field('location')?></p>
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php echo get_field('start_date')?>-<?php echo get_field('end_date')?></p>
+                    </div>
+                </a>
+    <?php } ?>
+    </div>
+    
 </div>
 
 <?php
