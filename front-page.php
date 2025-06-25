2740258bc0902b70d0baae9b0dbf0304e5c385f6
Vladys K <vldk0022@gmail.com>
2025-06-25 18:08:37 +0200

    added three item grid

--- front-page.php
+++ front-page.php
@@ -50,6 +50,49 @@ get_header();
     </div>
 
 </div>
+<div class="exhibitions-container">
+    <div>
+        <p>Exhibitions</p>
+    </div>
+    <div class="exhibitions-grid">
+        <?php
+        $args = [
+                'post_type' => 'exhibition', 
+                'posts_per_page' => 3,       
+            ];
+        $exhibitions = new WP_Query($args);
+
+        if ($exhibitions -> have_posts()){
+            while ($exhibitions -> have_posts()){
+            $exhibitions -> the_post()?>
+            <div class="exhibition-card">
+                    <div class="image-container">
+                        <?php
+                            if (has_post_thumbnail()) {
+                            $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                            echo '<img  src="' . esc_url($image_url) . '" alt="' . esc_attr(get_the_title()) . '">';
+                            }
+                        ?>
+                    </div>
+                    <div class="">
+                        <div>
+                            <span><?php echo get_field('location') ?></span>
+                            <h2><?php the_title();  ?></h2>
+                            <p><?php echo get_field('start_date') ?> - <?php echo get_field('end_date') ?></p>
+                        </div>
+                    </div>
+            </div>
+            
+            
+        <?php }
+           wp_reset_postdata();
+                } else {
+                    echo 'No exhibitions found';
+                }
+        ?>
+    </div>
+    </div>
+</div>
 <?php
  get_footer();
 ?>
