6bff61fdadf52467a5b0695bb7c7b19cd52fab35
Vladys K <vldk0022@gmail.com>
2025-06-25 12:27:27 +0200

    every card as a link

--- archive-news.php
+++ archive-news.php
@@ -10,12 +10,14 @@
               <?php
                 while (have_posts()) {
                 the_post();?>
-                <div class="news-grid__card">
-                    <img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="Thumbnail">
-                    <p class="card-type"><?php echo get_field('news_type'); ?></p>
-                    <h2><a href=""><?php the_title(); ?></a></h2>
-                    <p class="card-description"><?php echo get_field('description'); ?></p>
-                </div>
+                <a href="<?php the_permalink(); ?>" class="news-grid__card-link">
+                    <div class="news-grid__card">
+                        <img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="Thumbnail">
+                        <p class="card-type"><?php echo get_field('news_type'); ?></p>
+                        <h2><?php the_title(); ?></h2>
+                        <p class="card-description"><?php echo get_field('description'); ?></p>
+                    </div>
+                </a>
             <?php } ?>
     </div>
 </div>
