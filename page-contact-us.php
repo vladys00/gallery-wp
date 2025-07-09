6925d4e0b25d26bf9f7c7acfec854d73cdadc7c9
Vladys K <vldk0022@gmail.com>
2025-07-09 12:00:42 +0200

    changed contact form

--- page-contact-us.php
+++ page-contact-us.php
@@ -5,7 +5,7 @@ get_header();
 
 
 <div class="contact-form-wrapper">
-    <?php echo do_shortcode('[contact-form-7 id="f927268" title="Contact form 1"]');?>
+    <?php echo do_shortcode('[contact-form-7 id="14bf40c" title="simple contact form"]');?>
 </div>
 
 <?php
